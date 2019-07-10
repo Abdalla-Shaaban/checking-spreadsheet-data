f54d0c3efa82fea44b6cc276623fd64614e01f48
Abdalla Shaaban <49037376+Abdalla-Shaaban@users.noreply.github.com>
2019-07-10 10:40:18 -0400

    Update dataCollection.js

--- dataCollection.js
+++ dataCollection.js
@@ -1,51 +1,19 @@
-function saveHeadCounts() {
+function headcountQuery() {
+    // fetch the form response spreadsheet
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
     var values = sheet.getDataRange().getValues();
     var secondSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[1];
     var secondSheetValues = secondSheet.getDataRange().getValues();
-
-    //find the first row of current day
+    var today = Utilities.formatDate(new Date(), "GMT", "MM/dd/yyyy");
+    var monthYearFormat = Utilities.formatDate(new Date(), "GMT", "yyyy-MM");
+    var previousHCLabs = "0";
+    var previousHCHD = "0";
+    var newTotalHCHD = "0";
+    var newTotalHCLabs = "0";
+    var totalHeadCountsForLabs = "0";
+    var totalHeadCountsForHD = "0";
     var firstRow = 0;
     var dateMatched = false;
-    var today = Utilities.formatDate(new Date(), "ET", "MM/dd/yyyy");
-    var monthYearFormat = Utilities.formatDate(new Date(), "ET", "yyyy-MM");
-    var previousHC = 0;
-    var newTotalHC = 0;
-    var range;
-    for (var i = 0; i < values.length; i++) {
-        var date = Utilities.formatDate(new Date(values[i][0]), "ET", "MM/dd/yyyy");
-        if (date == today) {
-            firstRow = i;
-            break;
-        }
-    }
-    //cacluate the total head counts for the day
-    var totalHeadCounts = 0;
-    var headCounts = sheet.getRange(firstRow + 1, 5, sheet.getLastRow()).getValues();
-    for (var row in headCounts) {
-        for (var col in headCounts[row]) {
-            totalHeadCounts = totalHeadCounts + headCounts[row][col];
-        }
-    }
-    for (var i = 0; i < secondSheetValues.length; i++) {
-        if (Utilities.formatDate(new Date(secondSheetValues[i][1]), "ET", "yyyy-MM") == monthYearFormat) {
-            previousHC = secondSheet.getDataRange().getValues()[i][0];
-            range = secondSheet.getRange(i + 1, 1);
-            dateMatched = true;
-        }
-    }
-    //append new data to spreadsheet
-    if (dateMatched) {
-        newTotalHC = parseInt(previousHC) + parseInt(totalHeadCounts);
-        range.setValue(newTotalHC);
-    } else { secondSheet.appendRow([totalHeadCounts, Utilities.formatDate(new Date(), "ET", "yyyy-MM")]); }
-}
-
-function sendMissingLogs() {
-    // fetch the form response spreadsheet
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-    var values = sheet.getDataRange().getValues();
-    var firstRow = 0;
     var message = "";
     var noEntries = false;
     var noEntriesRBS = true;
@@ -61,11 +29,9 @@ function sendMissingLogs() {
     var ENGSchedule = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM'];
 
     //find the first row of current day
-    var today = Utilities.formatDate(new Date(), "ET", "MM/dd/yyyy");
     for (var i = 0; i < values.length; i++) {
-        var date = new Date(values[i][0]);
-        var formattedDate = Utilities.formatDate(date, "ET", "MM/dd/yyyy");
-        if (formattedDate == today) {
+        var date = Utilities.formatDate(new Date(values[i][0]), "GMT", "MM/dd/yyyy");
+        if (date == today) {
             firstRow = i;
             break;
         }
@@ -73,13 +39,17 @@ function sendMissingLogs() {
 
     //check if no entries was provided for the day
     if (firstRow == 0) { noEntries = true; }
+
     //check today's submissions
     if (!noEntries) {
+        //find missing headcounts
         var labLocation = sheet.getRange(firstRow + 1, 3, sheet.getLastRow()).getValues(); //(row, column, NumRows) 
         var timeStamps = sheet.getRange(firstRow + 1, 4, sheet.getLastRow()).getValues();
+        var headCounts = sheet.getRange(firstRow + 1, 5, sheet.getLastRow()).getValues();
         for (var row in labLocation) {
             for (var col in labLocation[row]) {
                 if (labLocation[row][col] == "RBS Lab") {
+                    totalHeadCountsForLabs = Number(totalHeadCountsForLabs) + Number(headCounts[row][col]);
                     noEntriesRBS = false;
                     for (i in RBSSchedule) {
                         if (timeStamps[row][col] == RBSSchedule[i]) {
@@ -88,6 +58,7 @@ function sendMissingLogs() {
                         }
                     }
                 } else if (labLocation[row][col] == "Dana Lab") {
+                    totalHeadCountsForLabs = Number(totalHeadCountsForLabs) + Number(headCounts[row][col]);
                     noEntriesDana = false;
                     for (i in danaSchedule) {
                         if (timeStamps[row][col] == danaSchedule[i]) {
@@ -96,6 +67,7 @@ function sendMissingLogs() {
                         }
                     }
                 } else if (labLocation[row][col] == "Hill Hall Lab") {
+                    totalHeadCountsForLabs = Number(totalHeadCountsForLabs) + Number(headCounts[row][col]);
                     noEntriesHill = false;
                     for (i in hillSchedule) {
                         if (timeStamps[row][col] == hillSchedule[i]) {
@@ -104,6 +76,7 @@ function sendMissingLogs() {
                         }
                     }
                 } else if (labLocation[row][col] == "Cyber Lounge") {
+                    totalHeadCountsForLabs = Number(totalHeadCountsForLabs) + Number(headCounts[row][col]);
                     noEntriesCyber = false;
                     for (i in cyberSchedule) {
                         if (timeStamps[row][col] == cyberSchedule[i]) {
@@ -112,6 +85,7 @@ function sendMissingLogs() {
                         }
                     }
                 } else if (labLocation[row][col] == "Engelhard Lab") {
+                    totalHeadCountsForLabs = Number(totalHeadCountsForLabs) + Number(headCounts[row][col]);
                     noEntriesENG = false;
                     for (i in ENGSchedule) {
                         if (timeStamps[row][col] == ENGSchedule[i]) {
@@ -120,55 +94,77 @@ function sendMissingLogs() {
                         }
                     }
                 } else if (labLocation[row][col] == "Help Desk") {
+                    totalHeadCountsForHD = Number(totalHeadCountsForHD) + Number(headCounts[row][col]);
                     noEntriesHD = false;
                 }
             }
         }
     }
+    //check second sheet for previos entries
+    for (var i = 0; i < secondSheetValues.length; i++) {
+        if (Utilities.formatDate(new Date(secondSheetValues[i][2]), "GMT", "yyyy-MM") == monthYearFormat) {
+            previousHCHD = secondSheet.getDataRange().getValues()[i][0];
+            previousHCLabs = secondSheet.getDataRange().getValues()[i][1];
+            rangeForHD = secondSheet.getRange(i + 1, 1);
+            rangeForLabs = secondSheet.getRange(i + 1, 2);
+            dateMatched = true;
+        }
+    }
+
+    Logger.log(totalHeadCountsForHD);
+    Logger.log(totalHeadCountsForLabs);
+    //append new data to second spreadsheet
+    if (dateMatched) {
+        newTotalHCHD = Number(previousHCHD) + Number(totalHeadCountsForHD);
+        newTotalHCLabs = Number(previousHCLabs) + Number(totalHeadCountsForLabs);
+        rangeForHD.setValue(newTotalHCHD);
+        rangeForLabs.setValue(newTotalHCLabs);
+    } else { secondSheet.appendRow([Number(totalHeadCountsForHD), Number(totalHeadCountsForLabs), Utilities.formatDate(new Date(), "GMT", "yyyy-MM")]); }
 
     //email content 
-    var emailAddress = "test@email.com"; // this could be any email you want it to be 
-    var subject = ("HeadCounts Report for " + today);
+    var emailAddress = "kmc342@rutgers.edu"; // this could be any email you want it to be 
+    var subject = ("Head Counts Report for " + today);
     if (!noEntries) {
         message = "---------------Missing Head Counts---------------";
     }
     if (!noEntriesRBS) {
         message = message + ("\n\nRBS Lab : " + RBSSchedule);
     } else {
-        message = message + ("\n\nRBS Lab : missing all entries");
+        message = message + ("\n\nRBS Lab : Missing all entries");
     }
 
     if (!noEntriesDana) {
         message = message + ("\n\nDana Library Lab : " + danaSchedule);
     } else {
-        message = message + ("\n\nDana Library Lab :  missing all entries");
+        message = message + ("\n\nDana Library Lab :  Missing all entries");
     }
 
     if (!noEntriesHill) {
         message = message + ("\n\nHill Hall Lab : " + hillSchedule);
     } else {
-        message = message + ("\n\nHill Hall Lab : missing all entries");
+        message = message + ("\n\nHill Hall Lab : Missing all entries");
     }
 
     if (!noEntriesCyber) {
         message = message + ("\n\nCyber Lounge : " + cyberSchedule);
     } else {
-        message = message + ("\n\nCyber Lounge : missing all entries");
+        message = message + ("\n\nCyber Lounge : Missing all entries");
     }
 
     if (!noEntriesENG) {
         message = message + ("\n\nEngelhard Lab : " + ENGSchedule);
     } else {
-        message = message + ("\n\nEngelhard Lab : missing all entries");
+        message = message + ("\n\nEngelhard Lab : Missing all entries");
     }
 
     if (!noEntriesHD) {
-        message = message + ("\n\nHelp Desk: today's entries were provided");
+        message = message + ("\n\nHelp Desk: Headcount provided");
     } else {
-        message = message + ("\n\nHelp Desk: missing todays head counts");
+        message = message + ("\n\nHelp Desk: Missing headcount");
     }
     if (noEntries) {
-        message = ("No head count entries provided for " + today);
+        message = ("No headcount entries were provided for all locations on" + today);
     }
+    //Logger.log(message);
     MailApp.sendEmail(emailAddress, subject, message);
 }
